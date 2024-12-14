751a2d3c79580b3421b3fae428723fceb5df94bf
Markucho18 <marcossosa21415@gmail.com>
2024-12-14 09:35:47 -0300

    Servidor escuchando(config basica)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Test")
+require("dotenv").config()
+const PORT = process.env.PORT || 3001
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+app.use(express.json())
+
+app.use(cors(
+//Esto configurarlo despues
+/* {
+  origin: "http://localhost:5173",
+  credentials: true
+} */
+))
+
+app.get("/", (req, res) => {
+  res.send("Consulta get xd")
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`)
+})
